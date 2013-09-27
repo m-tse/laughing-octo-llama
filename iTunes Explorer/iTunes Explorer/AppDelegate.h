80169f19031aec91cb341a32dfd22896a0c10a50
Tianyu Shi <ts133@duke.edu>
2013-09-27 01:04:51 -0400

    xcode work enviroment
    I wanted to start writing xcode stuff. this is just spritekit game
    setup.

---
+++ iTunes Explorer/iTunes Explorer/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iTunes Explorer
+//
+//  Created by Tianyu Shi on 9/27/13.
+//  Copyright (c) 2013 Tianyu Shi, Matt Tse, Andrew Shim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iTunes Explorer/iTunes Explorer/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  iTunes Explorer
+//
+
+//  Copyright (c) 2013 Tianyu Shi, Matt Tse, Andrew Shim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+
+@interface ViewController : UIViewController
+
+@end
