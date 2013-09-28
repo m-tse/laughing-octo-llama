94a1432b901efd6fb0e320a12f0bfbf971d3f60c
Andrew Shim <andrewkshim92@gmail.com>
2013-09-28 16:44:34 -0400

    particle emitter practice

--- nodeServer/parseData.js
+++ nodeServer/parseData.js
@@ -3,6 +3,7 @@ var querystring = require('querystring');
 var fs = require('fs');
 var Firebase = require('firebase');
 var itunes = require('searchitunes');
+var _ = require('underscore');
 
 var appleLookupApi = {
   host: 'itunes.apple.com',
@@ -11,8 +12,9 @@ var appleLookupApi = {
 }
 var database = new Firebase('https://itunesgalaxy.firebaseio.com/');
 var songs = database.child('songs');
+var genres = database.child('genres');
 
-function appleLookup(params, callback) {
+function appleLookup(params, attributes, count, callback) {
 
   var req = http.request({
     host: appleLookupApi.host,
@@ -30,7 +32,7 @@ function appleLookup(params, callback) {
       if(data.length >= 2 && data.substr(0,1) == '{' && data.substr( data.length -1, 1 ) == '}') {
         data = JSON.parse(data);
         if( data.resultCount !== undefined ) {
-          callback(data['results']);
+          callback(data['results'], attributes, count);
         } else {
           callback(false, {reason: 'incomplete json', headers: response.headers, data: data});
         }
@@ -42,25 +44,38 @@ function appleLookup(params, callback) {
 }
 
 if (true) {
+  var genreArray = []
   fs.readFile('./appleData/song_popularity_per_genre', 'utf8', function (err, data) {
     if (err) {
       return console.log(err);
     } 
     var count = 0;
     data.toString().split('\n').forEach(function(line) {
-      count += 1;
-      if (count > 10) {
-        return;
-      }
+/*      if (count > 10) {*/
+        //return;
+/*      }*/
       attributes = line.toString().split(/\s+/)
+      count += 1;
+
       appleLookup({
-        id: attributes[3] 
-      }, function(data) {
-        if (data.length > 0) {
+        id: attributes[3]
+      }, attributes, count, function(data, params, count) {
+        if (data.length > 0 && params.length > 1) {
           data = data[0];
-          data.id = attributes[3];
-          data.genre = attributes[2];
-          data.rank = attributes[4];
+          data["id"] = params[3];
+          data["genre"] = params[2];
+          data["rank"] = params[4];
+          if (!_.contains(genreArray, data.primaryGenreName) && data.genre) {
+            var newGenre = {};
+            genres.push({name: data.primaryGenreName});
+            genreArray.push(data.primaryGenreName);
+            console.log(genreArray);
+          }
+          if (count > 100) {
+            return;
+          }
+          console.log(params)
+          console.log(data)
           songs.push(data)
         }
       });
