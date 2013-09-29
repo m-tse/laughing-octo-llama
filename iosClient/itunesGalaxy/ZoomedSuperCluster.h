2c4c4eb4a990196f6ff8de4f59a787928cc38dff
mattse <matt.s.tse@gmail.com>
2013-09-28 20:59:34 -0400

    attempted to get gestures

---
+++ iosClient/itunesGalaxy/ZoomedSuperCluster.h
@@ -0,0 +1,13 @@
+//
+//  ZoomedSuperCluster.h
+//  itunesGalaxy
+//
+//  Created by Matthew on 9/28/13.
+//  Copyright (c) 2013 MTA. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface ZoomedSuperCluster : SKNode
+
+@end
---
+++ iosClient/itunesGalaxy/ZoomedSuperCluster.m
@@ -0,0 +1,13 @@
+//
+//  ZoomedSuperCluster.m
+//  itunesGalaxy
+//
+//  Created by Matthew on 9/28/13.
+//  Copyright (c) 2013 MTA. All rights reserved.
+//
+
+#import "ZoomedSuperCluster.h"
+
+@implementation ZoomedSuperCluster
+
+@end
