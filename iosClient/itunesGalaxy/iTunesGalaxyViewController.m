59aece45169935783aabd68b22fcd6d019de0571
Andrew Shim <andrewkshim92@gmail.com>
2013-09-29 01:47:39 -0400

    iTunesNode started

--- iosClient/itunesGalaxy/iTunesGalaxyViewController.m
+++ iosClient/itunesGalaxy/iTunesGalaxyViewController.m
@@ -22,7 +22,7 @@
     skView.showsNodeCount = YES;
     
     // Create and configure the scene.
-    SKScene * scene = [MainScene sceneWithSize:skView.bounds.size];
+    SKScene * scene = [TestScene sceneWithSize:skView.bounds.size];
     scene.scaleMode = SKSceneScaleModeAspectFill;
     
     // Present the scene.
---
+++ iosClient/itunesGalaxy/iTunesNode.h
@@ -0,0 +1,26 @@
+//
+//  iTunesNode.h
+//  itunesGalaxy
+//
+//  Created by Andrew Shim on 9/29/13.
+//  Copyright (c) 2013 MTA. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <SpriteKit/SpriteKit.h>
+
+@interface iTunesNode : NSObject
+
+@property iTunesNode *rootNode;
+@property NSArray *mediaTypes;
+@property iTunesNode *parent;
+@property NSMutableArray *children;
+@property SKScene *myScene;
+@property NSString *myName;
+
+-(id) init:(SKScene *)scene name:(NSString *)name;
++(id) iTunesRootNode;
+-(id) createRootNode;
+-(void) addChild:(iTunesNode *)child;
+
+@end
